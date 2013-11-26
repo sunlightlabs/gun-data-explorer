657922bf4d8eea36b759bef22d13fb97a790a5ed
Bob Lannon <blannon@gmail.com>
2013-11-26 10:54:43 -0500

    dynamically generate links to data.ie

--- bars.js
+++ bars.js
@@ -1,3 +1,7 @@
+var Base64={_keyStr:"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",encode:function(a){var b="";var c,chr2,chr3,enc1,enc2,enc3,enc4;var i=0;a=Base64._utf8_encode(a);while(i<a.length){c=a.charCodeAt(i++);chr2=a.charCodeAt(i++);chr3=a.charCodeAt(i++);enc1=c>>2;enc2=((c&3)<<4)|(chr2>>4);enc3=((chr2&15)<<2)|(chr3>>6);enc4=chr3&63;if(isNaN(chr2)){enc3=enc4=64}else if(isNaN(chr3)){enc4=64}b=b+this._keyStr.charAt(enc1)+this._keyStr.charAt(enc2)+this._keyStr.charAt(enc3)+this._keyStr.charAt(enc4)}return b},decode:function(a){var b="";var c,chr2,chr3;var d,enc2,enc3,enc4;var i=0;a=a.replace(/[^A-Za-z0-9\+\/\=]/g,"");while(i<a.length){d=this._keyStr.indexOf(a.charAt(i++));enc2=this._keyStr.indexOf(a.charAt(i++));enc3=this._keyStr.indexOf(a.charAt(i++));enc4=this._keyStr.indexOf(a.charAt(i++));c=(d<<2)|(enc2>>4);chr2=((enc2&15)<<4)|(enc3>>2);chr3=((enc3&3)<<6)|enc4;b=b+String.fromCharCode(c);if(enc3!=64){b=b+String.fromCharCode(chr2)}if(enc4!=64){b=b+String.fromCharCode(chr3)}}b=Base64._utf8_decode(b);return b},_utf8_encode:function(a){a=a.replace(/\r\n/g,"\n");var b="";for(var n=0;n<a.length;n++){var c=a.charCodeAt(n);if(c<128){b+=String.fromCharCode(c)}else if((c>127)&&(c<2048)){b+=String.fromCharCode((c>>6)|192);b+=String.fromCharCode((c&63)|128)}else{b+=String.fromCharCode((c>>12)|224);b+=String.fromCharCode(((c>>6)&63)|128);b+=String.fromCharCode((c&63)|128)}}return b},_utf8_decode:function(a){var b="";var i=0;var c=c1=c2=0;while(i<a.length){c=a.charCodeAt(i);if(c<128){b+=String.fromCharCode(c);i++}else if((c>191)&&(c<224)){c2=a.charCodeAt(i+1);b+=String.fromCharCode(((c&31)<<6)|(c2&63));i+=2}else{c2=a.charCodeAt(i+1);c3=a.charCodeAt(i+2);b+=String.fromCharCode(((c&15)<<12)|((c2&63)<<6)|(c3&63));i+=3}}return b}};
+
+var data_ie_base_url = 'http://data.influenceexplorer.com/contributions/#';
+
 var svg = d3.select('#chart').attr('width','100%').attr('height','100%');
 
 var labels = [  {'label':'Gun Control','key':'ANTI-GUN','x':0},
@@ -113,7 +117,7 @@ var xmax;
 var states;
 
 
-    states = d3.json('state_data.json', function(json) {
+    states = d3.json('state_data_update.json', function(json) {
         states = json.sort(function(a,b) { 
             return get_or_zero(b,'PRO-GUN') - get_or_zero(a,'PRO-GUN');})
         draw(states);
@@ -238,12 +242,19 @@ function clickState(d){
     }
 }
 
-function get_or_hash(d,k){
+function get_or_hash(d, state_fed, industry){
+    /*
     if (d.hasOwnProperty(k)){
         return d[k];
     } else {
         return '#'
-    }
+    }*/
+    params = 'contributor_industry=' + industry;
+    params += '&recipient_state=' + d.abbr;
+    params += '&general_transaction_type=standard';
+    params += '&transaction_namespace=' + state_fed;
+    console.log(params);
+    return data_ie_base_url + Base64.encode(params);
 }
 
 function selectState(d){
@@ -260,24 +271,24 @@ function selectState(d){
                 {   
                     stance: 'Rights'  , 
                     val : get_or_zero(d,'state_PRO-GUN'), 
-                    url : get_or_hash(d.urls,'state_PRO-GUN')
+                    url : get_or_hash(d,'urn%3Animsp%3Atransaction','Q13')
                 }, 
                 {   
                     stance: 'Control'  , 
                     val : get_or_zero(d,'state_ANTI-GUN'), 
-                    url : get_or_hash(d.urls,'state_ANTI-GUN')
+                    url : get_or_hash(d,'urn:nimsp:transaction','Q12')
                 } 
                     ];
     d_federal = [
                 {   
                     stance: 'Rights'  , 
                     val : get_or_zero(d,'federal_PRO-GUN'), 
-                    url : get_or_hash(d.urls,'federal_PRO-GUN')
+                    url : get_or_hash(d,'urn:fec:transaction','Q13')
                 }, 
                 {   
                     stance: 'Control'  , 
                     val : get_or_zero(d,'federal_ANTI-GUN'), 
-                    url : get_or_hash(d.urls,'federal_ANTI-GUN')
+                    url : get_or_hash(d,'urn:fec:transaction','Q12')
                 } 
                     ];
 
